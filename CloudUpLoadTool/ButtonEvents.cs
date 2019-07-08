e213059b503928c4a9bbda2598dd76c2df1229b1
Lewis <Lewis@DESKTOP-UC56A9R>
2019-07-08 10:35:07 +0800

    添加项目文件。

---
+++ CloudUpLoadTool/ButtonEvents.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using Kingdee.BOS.Core.Bill.PlugIn;
+using Kingdee.BOS.Core.DynamicForm.PlugIn.Args;
+
+namespace CloudUpLoadTool
+{
+    public class ButtonEvents : AbstractBillPlugIn
+    {
+        public override void BarItemClick(BarItemClickEventArgs e)
+        {
+            var resultMessage = string.Empty;
+
+            //订单退回操作
+            base.BarItemClick(e);
+
+            if (e.BarItemKey == "tbUpLoadOrder")
+            {
+                //定义获取表头信息对像
+                var docScddIds1 = View.Model.DataObject;
+                //获取表头中单据编号信息(注:这里的BillNo为单据编号中"绑定实体属性"项中获得)
+                var dhstr = docScddIds1["BillNo"].ToString();
+
+                fcy.Service.CnnStr = "http://172.16.4.252/websys/service.asmx";
+                fcy.Service.userdmstr = "feng";
+                fcy.Service.passwordstr = "";
+
+                //将获取的单据名称进行截取,取前两位
+                var orderno = dhstr.Substring(0,2);
+                //根据获取的标记分别进行单据上传至U订货相关单据
+                switch (orderno)
+                {
+                    //应收单
+                    case "AR":
+                        resultMessage = UploadOrder(dhstr);
+                        break;
+                }
+                //输出结果
+                View.ShowMessage(resultMessage);
+            }
+        }
+
+        /// <summary>
+        /// 将K3 CLOUD应收单上传至U订货发货单
+        /// </summary>
+        /// <param name="orderno"></param>
+        /// <returns></returns>
+        private string UploadOrder(string orderno)
+        {
+            var result = string.Empty;
+            try
+            {
+                result = FcyUdhPosts.Xsfhup(orderno);
+            }
+            catch (Exception ex)
+            {
+                result = ex.Message;
+            }
+            return result;
+        }
+
+        #region Hide Del function
+        /// <summary>
+        /// 执行删除销售订单
+        /// </summary>
+        /// <param name="orderno"></param>
+        /// <returns></returns>
+        //private string DelSalesOrder(string orderno)
+        //{
+        //    var ab = new Service();
+        //    var result = string.Empty;
+
+        //    var udhstr = ab.str("select udh from axsdd where dh='" + orderno + "'");
+        //    if (udhstr.Trim().Length > 0)
+        //    {
+        //        ab.sqlcmd("delete uddzy where cOrderNo='" + udhstr + "'");
+        //        ab.sqlcmd("delete uOrder where cOrderNo='" + udhstr + "'");
+        //        FcyUdhPosts.Ddht(udhstr);
+
+        //        result = $"{orderno}已在U订货平台内删除";
+        //    }
+        //    else
+        //    {
+        //        result = $"没有在U订货平台查找到{orderno}单据记录,故没有执行删除操作";
+        //    }
+        //    return result;
+        //}
+
+        ///// <summary>
+        ///// 删除收款退款单
+        ///// </summary>
+        ///// <param name="orderno"></param>
+        ///// <returns></returns>
+        //private string DelReturnOrder(string orderno)
+        //{
+        //    var result = string.Empty;
+
+        //    try
+        //    {
+        //        result = FcyUdhPosts.Tkddel(orderno);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        result = ex.Message;
+        //    }
+        //    return result;
+        //}
+
+        ///// <summary>
+        ///// 删除应收单
+        ///// </summary>
+        ///// <param name="orderno"></param>
+        ///// <returns></returns>
+        //private string DelReceive(string orderno)
+        //{
+        //    var result = string.Empty;
+
+        //    try
+        //    {
+        //        result = FcyUdhPosts.Xsfhdelup(orderno);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        result = ex.Message;
+        //    }
+        //    return result;
+        //}
+        #endregion
+    }
+}
---
+++ CloudUpLoadTool/FcyUdhPosts.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace CloudUpLoadTool
+{
+    public class FcyUdhPosts
+    {
+        /// <summary>
+        /// 将K3 CLOUD的应收单上传至U订货发货单
+        /// </summary>
+        /// <param name="dhstr"></param>
+        /// <returns></returns>
+        public static string Xsfhup(string dhstr)
+        {
+            var ret = "";
+            fcydata.Service ab = new fcydata.Service();
+            var data = ab.FDataSet("select dh,udh,rq,fhrq,shr,shtel,shaddr,hydh,hysj,hytel,hyrq,hyqd,hymz,hydz,hyyf,hyjs,fid,memo from fhd where dh='" + dhstr + "'");
+            var date = DateTime.Now;
+            var date1 = DateTime.Now;
+            var udhstr = data.Tables[0].Rows[0]["udh"].ToString().Trim();
+            Xsfhdelup(dhstr);
+            //   FcyUdhGet.xsdddmx(udhstr);
+            var datas = ab.FDataSet("select dh,wlbh,sl,dj,je,idkey from fhdsv where dh='" + dhstr + "'");
+
+            Dictionary<string, string> param = new Dictionary<string, string>();
+
+            string delivery = @"{<M>,oDeliveryDetails:[<S>]}";
+
+            string m = @" 'cOrderNo': '<udh>','cOutSysKey':'<dh>','dCreated':'<rq>','dSendDate' :'<fhrq>','cReceiveAddress': '<shaddr>',
+                          'cReceiver': '<shr>','cReceiveMobile': '<shtel>','oShippingMemo': {'remark': '<memo>'}";
+            try
+            {
+                date = (DateTime)data.Tables[0].Rows[0]["rq"];
+            }
+            catch
+            {
+            }
+            try
+            {
+                date1 = (DateTime)data.Tables[0].Rows[0]["fhrq"];
+            }
+            catch
+            {
+            }
+
+            m = m.Replace("<rq>", date.ToString("yyyy-MM-dd hh:mm:ss"));
+            m = m.Replace("<fhrq>", date1.ToString("yyyy-MM-dd hh:mm:ss"));
+            for (var i = 0; i < data.Tables[0].Columns.Count; i++)
+            {
+                m = m.Replace("<" + data.Tables[0].Columns[i].ColumnName + ">", data.Tables[0].Rows[0][i].ToString().Trim());
+            }
+
+            //        string s = @"{
+            //'iOrderDetailId': '<uid>',
+            // 'iSendQuantity': <sl>
+
+            // }";
+
+            var s = @"{'iOrderDetailId': '<uid>','iSendQuantity': <sl>,'fErpTransactionPrice': <dj>,'fErpSalePayMoney':<je>}";
+
+            var s1 = s;
+            var S = "";
+
+            for (int i = 0; i < datas.Tables[0].Rows.Count; i++)
+            {
+                s1 = s;
+                s1 = s1.Replace("<uid>", datas.Tables[0].Rows[i]["idkey"].ToString().Trim());
+                s1 = s1.Replace("<sl>", datas.Tables[0].Rows[i]["sl"].ToString().Trim());
+                s1 = s1.Replace("<dj>", datas.Tables[0].Rows[i]["dj"].ToString().Trim());
+                s1 = s1.Replace("<je>", datas.Tables[0].Rows[i]["je"].ToString().Trim());
+                S = S + s1 + ",";
+            }
+            S = S.Substring(0, S.Length - 1);
+            delivery = delivery.Replace("<M>", m);
+            delivery = delivery.Replace("<S>", S);
+            param.Add("delivery", delivery);
+
+            ret = FcyWeb.Post("/ws/Orders/saveDelivery", param);
+
+
+            var fhdh = "";
+            var xmldoc = new XmlDocument();
+            xmldoc.LoadXml(ret);
+            if (fcydata.FcyXml.GetNodeVal(xmldoc.DocumentElement, "code") == "200")
+            {
+                fhdh = fcydata.FcyXml.GetNodeVal(xmldoc.DocumentElement, "data/models.voucher.DeliveryVoucher/cDeliveryNo");
+            }
+            else
+                return ret;
+
+            //hydh,hysj,hytel,hyrq,hyqd,hymz,hydz,hyyf,hyjs,
+            param = new Dictionary<string, string>();
+            var json = @"{deliveryNo: '" + fhdh + "',outSysNo:'<dh>',deliveryDefine:{define1:'<hyjs>',define2:'<hymz>',define3:'<hyyf>'," +
+                       "define4:'<hydz>',define5:'<hydh>',define6:'<hysj>',define7:'<hytel>',define8:'<hyrq>'}}";
+            for (int i = 0; i < data.Tables[0].Columns.Count; i++)
+            {
+                json = json.Replace("<" + data.Tables[0].Columns[i].ColumnName + ">", data.Tables[0].Rows[0][i].ToString().Trim());
+            }
+            param.Add("deliveryDefineVo", json);
+            ret = FcyWeb.Post("/ws/Orders/saveDeliveryDefineVoApi", param);
+
+            ab.sqlcmd("insert into upfhd (dh) values ('" + dhstr + "')");
+
+            return ret;
+        }
+
+        /// <summary>
+        /// 删除U订货上的"发货通知单"(k3对应:应收单)
+        /// </summary>
+        /// <param name="cOutSysKey"></param>
+        /// <returns></returns>
+        public static string Xsfhdelup(string cOutSysKey)
+        {
+            var ret = "";
+            Dictionary<string, string> param = new Dictionary<string, string>();
+            param.Add("cOutSysKey", cOutSysKey);
+            ret = FcyWeb.Post("/ws/Orders/delDelivery", param);
+            return ret;
+        }
+
+        //public static string xsfhdelup(string cOutSysKey)
+        //{
+        //    string ret = "";
+        //    Dictionary<string, string> param = new Dictionary<string, string>();
+        //    param.Add("cOutSysKey", cOutSysKey);
+        //    ret = FcyWeb.Post("/ws/Orders/delDelivery", param);
+        //    return ret;
+        //}
+
+        #region Hide
+        /// <summary>
+        /// 删除U订货上的"退款单"(K3对应:收款退款单 及 其他应收单)
+        /// </summary>
+        /// <param name="cOutSysKey"></param>
+        /// <returns></returns>
+        public static string Tkddel(string cOutSysKey)
+        {
+            string ret = "";
+            Dictionary<string, string> param = new Dictionary<string, string>();
+            param.Add("outsyskey", cOutSysKey);
+            ret = FcyWeb.Post("/ws/Payments/delRefund", param);
+            return ret;
+        }
+
+        /// <summary>
+        /// 删除付款单
+        /// </summary>
+        /// <param name="cOutSysKey"></param>
+        /// <returns></returns>
+        public static string Zfddel(string cOutSysKey)
+        {
+            string ret = "";
+            Dictionary<string, string> param = new Dictionary<string, string>();
+            param.Add("cOutSysKey", cOutSysKey);
+            ret = FcyWeb.Post("/ws/Payments/delPayment", param);
+            return ret;
+        }
+
+
+
+        /// <summary>
+        /// 订单 回退(k3:销售订单使用)
+        /// </summary>
+        /// <param name="dh"></param>
+        /// <returns></returns>
+        public static string Ddht(string dh)
+        {
+            string ret = "";
+            Dictionary<string, string> param = new Dictionary<string, string>();
+            param.Add("orderno", dh);
+            ret = FcyWeb.Post("/ws/Orders/orderConfirmBackApi", param);
+            return ret;
+        }
+
+        /// <summary>
+        /// 上传应收单
+        /// </summary>
+        /// <param name="row"></param>
+        /// <returns></returns>
+        public static string Ysdup(DataRow row)
+        {
+            string ret = "";
+            fcydata.Service ab = new fcydata.Service();
+            if (row["je"].ToString().Trim().IndexOf("-") > 0)
+            {
+                row["je"] = -(decimal)row["je"];
+                MessageBox.Show(row["je"].ToString());
+                Dictionary<string, string> param = new Dictionary<string, string>();
+
+                DateTime date = (DateTime)row["rq"];
+                string strs = "{'iAmount':<je>,'cOutSysKey':'<dh>','oAgent':{'cCode':'<khbh>','cOutSysKey':'<khbh>'},'oSettlementWay':{'cErpCode':'<jsfs>'},'iPayMentStatusCode':2,'cRefundPayDirection': 'TOUDH','dPayFinishDate':'" + date.ToString("yyyy-MM-dd HH:mm:ss") + "','dReceiptDate':'" + date.ToString("yyyy-MM-dd HH:mm:ss") + "','remark':'<memo>'}";
+
+                for (int j = 0; j < row.Table.Columns.Count; j++)
+                {
+                    strs = strs.Replace("<" + row.Table.Columns[j].ColumnName + ">", row[j].ToString().Trim());
+                }
+                param.Add("refund", strs);
+                ret = FcyWeb.Post("/ws/Payments/saveRefund", param);
+                XmlDocument xmldoc = new XmlDocument();
+                xmldoc.LoadXml(ret);
+                if (fcydata.FcyXml.GetNodeVal(xmldoc.DocumentElement, "data") != "")
+                {
+                    MessageBox.Show(ret);
+                }
+
+
+
+            }
+            else
+            {
+                Dictionary<string, string> param = new Dictionary<string, string>();
+
+                DateTime date = (DateTime)row["rq"];
+                string strs = "{'iAmount':<je>,'cOutSysKey':'<dh>','oAgent':{'cErpCode':'<khbh>'},'oSettlementWay':{'cErpCode':'<jsfs>'},'cVoucherType':'NORMAL','iPayMentStatusCode':2,'dPayFinishDate':'" + date.ToString("yyyy-MM-dd HH:mm:ss") + "','dReceiptDate':'" + date.ToString("yyyy-MM-dd HH:mm:ss") + "','remark':'<memo>'}";
+
+                for (int j = 0; j < row.Table.Columns.Count; j++)
+                {
+                    strs = strs.Replace("<" + row.Table.Columns[j].ColumnName + ">", row[j].ToString().Trim());
+                }
+                param.Add("payment", strs);
+
+                ret = FcyWeb.Post("/ws/Payments/savePayment", param);
+                XmlDocument xmldoc = new XmlDocument();
+                xmldoc.LoadXml(ret);
+                if (fcydata.FcyXml.GetNodeVal(xmldoc.DocumentElement, "data") != "")
+                {
+                    MessageBox.Show(ret);
+                }
+            }
+            return ret;
+        }
+        #endregion
+    }
+}
